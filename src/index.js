7441530c2068767b9af19a996526d54d9d8e0dc7
George Benjamin <msdcconnect@gmail.com>
2020-06-10 08:32:11 +0100

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
-lol
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.status(200).json({ message: 'Welcome to my awesome Node app' }));
 
+// eslint-disable-next-line no-unused-vars
 app.use((req, res, next) => res.status(404).json({ message: 'The resource you are requesting for is not found' }));
 
 app.listen(port, () => console.log(`App started on port ${port}`));
